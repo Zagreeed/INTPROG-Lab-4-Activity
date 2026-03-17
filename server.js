c4ae151dd6ac9516c519dff087ece81837de1199
Danley Yap, Galan <galandanley701254@gmail.com>
2026-03-17 22:14:18 +0800

    added some changes

--- server.js
+++ server.js
@@ -21,13 +21,17 @@ app.use(express.json())
 let users = [
     {
         id: 1,
-        username: "admin",
+        firstname: "admin",
+        lastname: "user",
+        email: "admin@example.com",
         password: "$2b$10$aPbnWecBMzkQdNlVoNRIouuFU3o/F6Klg4v1.FIEhb.VOE7q98XdC",
         role: "admin"
     },
     {
         id: 2,
-        username: "alice",
+        firstname: "alice",
+        lastname: "go",
+        email: "user@email.com",
         password: "$2b$10$jg1uY57jx7pPMcyap0e6yeyb3HpkISic2RXQq1KCj73nUI.P.t23i",
         role: "user"
     }
@@ -89,6 +93,59 @@ app.post("/api/login", async (req, res) => {
     return res.status(200).json({ token, user: { username: user.username, role: user.role } })
 
 })
+
+
+
+
+
+// PROTECTED ROUTES
+app.get("/api/profile", authenticateToken, async (req, res) => {
+    res.json({ user: req.user })
+})
+
+
+app.get("/api/admin/dashbaord", authenticateToken, authorizeRole("admin"), (req, res) => {
+    res.json({ message: "Welcome to admin DashBaord", data: "Secret admin info" })
+})
+
+
+// PUBLIC ROUTE
+app.get("/api/content/guest", (req, res) => {
+    res.status(200).json({ message: "Public content all visitors" })
+})
+
+
+
+
+/// MIDDLEWARES
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({ error: "Access Token Required" })
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ error: "Invalid or expire token" })
+
+        req.user = user
+
+        next()
+    })
+}
+
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ error: "Access denied: Insufficient Permission" })
+        }
+
+        next()
+    }
+}
 
 
 
